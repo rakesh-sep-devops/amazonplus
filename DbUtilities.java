15597d867c7fec3d36972849b77b0f548d6f440e
Rakeesh07kumar <rakeshkumar169212@gmail.com>
2022-09-07 23:22:45 +0530

    first commit for amazonproject

---
+++ DbUtilities.java
@@ -0,0 +1,6 @@
+this is first script for the amazonplus_project
+echo "this is the first script of the project ---> amazonplus_project"
+echo " hello"
+echo "goood morning"
+echo " Today date is " date
+
---
+++ MainframeUtilities.java
@@ -0,0 +1,6 @@
+
+this is the second shell script for the project amazonplus_project
+echo "this is second script "
+
+echo "welcome  to 2nd script"
+
